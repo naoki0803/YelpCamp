d9937dbe2d686e237117d5b2af82c9f4e416c218
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-06-12 06:17:19 +0900

    489. エラー時のフラッシュパーシャル

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -47,12 +47,21 @@ router.post("/", validateCampground, catchAsync(async (req, res) => {
 //詳細ページ
 router.get("/:id", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'キャンプ場は見つかりませんでした');
+        return res.redirect("/campgrounds")
+    }
     res.render("campgrounds/show", { campground })
 }));
 
 //編集ページ
 router.get("/:id/edit", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'キャンプ場は見つかりませんでした');
+        res.redirect('/campgrounds')
+
+    }
     res.render("campgrounds/edit", { campground })
 }));
 
