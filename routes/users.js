2b090b0c70daebf9bddda6bc6163b36a394e2107
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-06-22 14:30:27 +0900

    510. ログアウトの実装

--- routes/users.js
+++ routes/users.js
@@ -32,7 +32,21 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect('/campgrounds');
 });
 
+// router.get('/logout', (req, res) => {
+//     req.logout();
+//     req.flash('success', 'ログアウトしました');
+//     res.redirect('/campgrounds');
+// });
 
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'ログアウトしました');
+        res.redirect('/campgrounds');
+    });
+});
 
 
 module.exports = router;
