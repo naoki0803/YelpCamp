d4168c07078a49a5e8a62cd69eea15880af04ec5
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-06-22 14:59:15 +0900

    511. currentUserヘルパー

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log("req.user", req.user);
     if(!req.isAuthenticated()){
         req.flash('error', 'ログインしてください');
         return res.redirect('/login');
