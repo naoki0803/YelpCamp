fa781d60dca251562b7bd1ef1ff394aa8b5e1430
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-07-07 06:04:30 +0900

    543. キャンプ場を地図の中心にもってくる

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,12 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 1, // starting zoom
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 11, // starting zoom
 });
+
+
+    // Create a default Marker and add it to the map.
+    const marker1 = new mapboxgl.Marker()
+        .setLngLat(campground.geometry.coordinates)
+        .addTo(map);
