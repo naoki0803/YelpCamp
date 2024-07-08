e1566886f9fe3ec9e20c6b9a7c7d4322bd50037e
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-07-09 05:27:57 +0900

    548. キャンプ場のクラスタリング

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,13 +1,20 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    // style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    // style: 'mapbox://styles/mapbox/navigation-night-v1',
+    style: 'mapbox://styles/mapbox/satellite-streets-v12',
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 11, // starting zoom
 });
 
 
-    // Create a default Marker and add it to the map.
-    const marker1 = new mapboxgl.Marker()
-        .setLngLat(campground.geometry.coordinates)
-        .addTo(map);
+// Create a default Marker and add it to the map.
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+        .setHTML(`<h5>${campground.title}</h5><p>${campground.location}</p> `)
+    )
+
+    .addTo(map);
