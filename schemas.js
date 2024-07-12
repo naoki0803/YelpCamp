b828ada4d3e5d464ca41f3c7128eb30131d1f2fc
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-07-13 05:54:07 +0900

    560. JOIでHTMLのサニタイズ

--- schemas.js
+++ schemas.js
@@ -1,19 +1,45 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
-        location: Joi.string().required(),
-        description: Joi.string().required(),
-        // image: Joi.string().required()  // この行を追加
+        // image: Joi.string().required(),
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML(),
     }).required(),
-    deleteImages: Joi.array() 
+    deleteImages: Joi.array()
 });
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        raiting: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required().escapeHTML()
     }).required()
 });
+
+//escapeHTML()を呼び出す際に、rules.escapeHTML()と呼び出さなくても利用できる理由
+//https://chatgpt.com/share/bdcf20cf-d14d-4b27-afa9-d3f17dc2bb85
